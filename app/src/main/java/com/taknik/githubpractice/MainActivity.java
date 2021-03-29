ad92908a81c5b7e0c6c81d1c6839c2db313cf478
shazadanjum2 <shahzad.anjum@taknik.com>
2021-03-29 03:52:27 -0700

    Initial test

---
+++ app/src/main/java/com/taknik/githubpractice/MainActivity.java
@@ -0,0 +1,18 @@
+package com.taknik.githubpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "test sourcetree", Toast.LENGTH_SHORT).show();
+
+    }
+}
